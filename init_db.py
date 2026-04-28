938fa08dbadffaecfb503b40cb790aac59b7c6c3
Sakshi Awasthi <sakshiawasthi1411@gmail.com>
2026-04-28 14:40:07 +0530

    Keep only one admin seed account

--- init_db.py
+++ init_db.py
@@ -3,10 +3,8 @@ from extensions import db
 from models import User
 from utils import generate_consumer_number
 
-
 ADMIN_EMAIL = 'admin@bsphcl.gov.in'
 ADMIN_PASSWORD = 'Admin@2024'
-
 
 def seed_admin():
     admin = User.query.filter_by(email=ADMIN_EMAIL).first()
@@ -29,7 +27,6 @@ def seed_admin():
         admin.department = 'Central Control'
 
     db.session.commit()
-
 
 with app.app_context():
     db.create_all()
